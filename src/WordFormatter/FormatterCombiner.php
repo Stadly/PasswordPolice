b04e783959e336cde87845b77d4b460846209343
Stadly <magnar@myrtveit.com>
2019-01-22 10:21:43 +0100

    Refactor unique filtering in formatter combiner

--- src/WordFormatter/FormatterCombiner.php
+++ src/WordFormatter/FormatterCombiner.php
@@ -15,23 +15,21 @@ final class FormatterCombiner extends ChainableFormatter
     private $wordFormatters;
 
     /**
-     * @var bool Whether the result should only contain unique words.
-     */
-    private $filterUnique;
-
-    /**
      * @param WordFormatter[] $wordFormatters Word formatters.
      * @param bool $includeUnformatted Whether the result should also include the words unformatted.
      * @param bool $filterUnique Whether the result should only contain unique words.
      */
     public function __construct(array $wordFormatters, bool $includeUnformatted = true, bool $filterUnique = true)
     {
-        if ($includeUnformatted) {
+        if ($filterUnique) {
+            $formatterCombiner = new FormatterCombiner($wordFormatters, $includeUnformatted, false);
+            $formatterCombiner->setNext(new UniqueFilter());
+            $wordFormatters = [$formatterCombiner];
+        } elseif ($includeUnformatted) {
             $wordFormatters[] = new Unformatter();
         }
 
         $this->wordFormatters = $wordFormatters;
-        $this->filterUnique = $filterUnique;
     }
 
     /**
@@ -39,22 +37,6 @@ final class FormatterCombiner extends ChainableFormatter
      * @return Traversable<string> The words formatted by all the word formatters in the formatter combiner.
      */
     protected function applyCurrent(iterable $words): Traversable
-    {
-        $formatted = $this->applyFormatters($words);
-
-        if ($this->filterUnique) {
-            $uniqueFilter = new UniqueFilter();
-            yield from $uniqueFilter->apply($formatted);
-        } else {
-            yield from $formatted;
-        }
-    }
-
-    /**
-     * @param iterable<string> $words Words to format.
-     * @return Traversable<string> The words formatted by all the word formatters in the formatter combiner.
-     */
-    private function applyFormatters(iterable $words): Traversable
     {
         foreach ($this->wordFormatters as $wordFormatter) {
             yield from $wordFormatter->apply($words);
