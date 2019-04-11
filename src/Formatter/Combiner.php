bf715190e444d8b602a5063c1458aecf712239e4
Stadly <magnar@myrtveit.com>
2019-04-11 17:12:57 +0200

    Combiner for combining multiple character tree formatters

---
+++ src/Formatter/Combiner.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\Formatter;
+
+use Stadly\PasswordPolice\CharTree;
+use Stadly\PasswordPolice\Formatter;
+
+final class Combiner implements Formatter
+{
+    use Chaining;
+
+    /**
+     * @var Formatter[] Character tree formatters.
+     */
+    private $formatters;
+
+    /**
+     * @var bool Whether the result should also include the character tree unformatted.
+     */
+    private $includeUnformatted;
+
+    /**
+     * @param Formatter[] $formatters Character tree formatters.
+     * @param bool $includeUnformatted Whether the result should also include the character tree unformatted.
+     */
+    public function __construct(array $formatters, bool $includeUnformatted = true)
+    {
+        $this->formatters = $formatters;
+        $this->includeUnformatted = $includeUnformatted;
+    }
+
+    /**
+     * @param CharTree $charTree Character tree to format.
+     * @return CharTree The character tree formatted by all the formatters in the combiner.
+     */
+    protected function applyCurrent(CharTree $charTree): CharTree
+    {
+        $charTrees = [];
+
+        if ($this->includeUnformatted) {
+            $charTrees[] = $charTree;
+        }
+
+        foreach ($this->formatters as $formatter) {
+            $charTrees[] = $formatter->apply($charTree);
+        }
+
+        return CharTree::fromArray($charTrees);
+    }
+}
---
+++ tests/Formatter/CombinerTest.php
@@ -0,0 +1,129 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\Formatter;
+
+use PHPUnit\Framework\TestCase;
+use Stadly\PasswordPolice\CharTree;
+use Stadly\PasswordPolice\Formatter;
+
+/**
+ * @coversDefaultClass \Stadly\PasswordPolice\Formatter\Combiner
+ * @covers ::<private>
+ * @covers ::<protected>
+ * @covers ::__construct
+ */
+final class CombinerTest extends TestCase
+{
+    /**
+     * @var Formatter
+     */
+    private $formatter1;
+
+    /**
+     * @var Formatter
+     */
+    private $formatter2;
+
+    protected function setUp(): void
+    {
+        $this->formatter1 = $this->createMock(Formatter::class);
+        $this->formatter1->method('apply')->willReturnCallback(
+            static function (CharTree $charTree): CharTree {
+                $charTrees = [];
+                foreach ($charTree as $string) {
+                    $charTrees[] = CharTree::fromString(strrev($string));
+                }
+                return CharTree::fromArray($charTrees);
+            }
+        );
+
+        $this->formatter2 = $this->createMock(Formatter::class);
+        $this->formatter2->method('apply')->willReturnCallback(
+            static function (CharTree $charTree): CharTree {
+                $charTrees = [];
+                foreach ($charTree as $string) {
+                    $charTrees[] = CharTree::fromString(strtolower($string));
+                    $charTrees[] = CharTree::fromString(strtoupper($string));
+                }
+                return CharTree::fromArray($charTrees);
+            }
+        );
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanFormatCharacterTree(): void
+    {
+        $formatter = new Combiner([$this->formatter1, $this->formatter2], false);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString('oOf'),
+            CharTree::fromString('RaB'),
+            CharTree::fromString('foo'),
+            CharTree::fromString('bar'),
+            CharTree::fromString('FOO'),
+            CharTree::fromString('BAR'),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanFormatCharacterTreeAndIncludeUnformatted(): void
+    {
+        $formatter = new Combiner([$this->formatter1, $this->formatter2], true);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('oOf'),
+            CharTree::fromString('RaB'),
+            CharTree::fromString('foo'),
+            CharTree::fromString('bar'),
+            CharTree::fromString('FOO'),
+            CharTree::fromString('BAR'),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanApplyFormatterChain(): void
+    {
+        $formatter = new Combiner([$this->formatter1, $this->formatter2], false);
+
+        $next = $this->createMock(Formatter::class);
+        $next->method('apply')->willReturnCallback(
+            static function (CharTree $charTree): CharTree {
+                $charTrees = [];
+                foreach ($charTree as $string) {
+                    $charTrees[] = CharTree::fromString(strrev($string));
+                }
+                return CharTree::fromArray($charTrees);
+            }
+        );
+
+        $formatter->setNext($next);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('oof'),
+            CharTree::fromString('rab'),
+            CharTree::fromString('OOF'),
+            CharTree::fromString('RAB'),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+        ])));
+    }
+}
