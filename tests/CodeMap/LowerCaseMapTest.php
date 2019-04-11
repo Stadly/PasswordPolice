c990b8888f2139aba7ab627781e065b57dad482e
Stadly <magnar@myrtveit.com>
2019-04-11 17:26:52 +0200

    Tests for lower case code map

---
+++ tests/CodeMap/LowerCaseMapTest.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\CodeMap;
+
+use PHPUnit\Framework\TestCase;
+use Stadly\PasswordPolice\CharTree;
+
+/**
+ * @coversDefaultClass \Stadly\PasswordPolice\CodeMap\LowerCaseMap
+ * @covers ::<private>
+ * @covers ::<protected>
+ */
+final class LowerCaseMapTest extends TestCase
+{
+    /**
+     * @covers ::getMap
+     */
+    public function testCanGetCodeMap(): void
+    {
+        $codeMap = new LowerCaseMap();
+
+        $charTree = CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString('123'),
+            CharTree::fromString('áÁ'),
+            CharTree::fromString('æ'),
+            CharTree::fromString('Øô'),
+            CharTree::fromString('Ëñ'),
+        ]);
+        self::assertEquals([
+            'f' => ['f'],
+            'B' => ['b'],
+            '1' => ['1'],
+            'á' => ['á'],
+            'æ' => ['æ'],
+            'Ø' => ['ø'],
+            'Ë' => ['ë'],
+        ], $codeMap->getMap($charTree), '', 0, 10, true);
+    }
+}
