ba420a06a9a2562363f113eb153019314a0237e9
Stadly <magnar@myrtveit.com>
2019-04-11 18:30:02 +0200

    Code map mapping leetspeak, working on character trees

---
+++ src/CodeMap/LeetspeakMap.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\CodeMap;
+
+use Stadly\PasswordPolice\CharTree;
+use Stadly\PasswordPolice\CodeMap;
+
+final class LeetspeakMap implements CodeMap
+{
+    private const ENCODE_MAP = [
+        'A' => ['4', '@', '∂'],
+        'B' => ['8', 'ß'],
+        'C' => ['(', '¢', '<', '[', '©'],
+        'D' => ['∂'],
+        'E' => ['3', '€', 'є'],
+        'F' => ['ƒ'],
+        'G' => ['6', '9'],
+        'H' => ['#'],
+        'I' => ['1', '!', '|', ':'],
+        'J' => ['¿'],
+        'K' => ['X'],
+        'L' => ['1', '£', 'ℓ'],
+        'O' => ['0', '°'],
+        'R' => ['2', '®', 'Я'],
+        'S' => ['5', '$', '§'],
+        'T' => ['7', '†'],
+        'U' => ['µ'],
+        'W' => ['vv'],
+        'X' => ['×'],
+        'Y' => ['φ', '¥'],
+        'Z' => ['2', '≥'],
+    ];
+
+    /**
+     * @var array<int, array<string|int, string[]>> Leetspeak code map.
+     */
+    private $codeMap = [];
+
+    /**
+     * @param bool $encode Whether the map should encode or decode Leetspeak.
+     */
+    public function __construct($encode = false)
+    {
+        if ($encode) {
+            foreach (self::ENCODE_MAP as $char => $codedChars) {
+                $this->codeMap[mb_strlen($char)][mb_strtoupper($char)] = $codedChars;
+            }
+        } else {
+            $codeMap = [];
+            foreach (self::ENCODE_MAP as $char => $codedChars) {
+                foreach ($codedChars as $codedChar) {
+                    $this->codeMap[mb_strlen($codedChar)][mb_strtoupper($codedChar)][] = $char;
+                }
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMap(CharTree $charTree): array
+    {
+        $codeMap = [];
+
+        // Add coded characters.
+        foreach ($this->codeMap as $length => $chars) {
+            foreach ($charTree->getTreeTrimmedToLength($length) as $char) {
+                if (isset($chars[mb_strtoupper($char)])) {
+                    $codeMap[$char] = $chars[mb_strtoupper($char)];
+                }
+            }
+        }
+
+        // Add uncoded characters.
+        foreach ($charTree->getTreeTrimmedToLength(1) as $char) {
+            $codeMap[$char][] = $char;
+        }
+
+        return $codeMap;
+    }
+}
---
+++ tests/CodeMap/LeetspeakMapTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\CodeMap;
+
+use PHPUnit\Framework\TestCase;
+use Stadly\PasswordPolice\CharTree;
+
+/**
+ * @coversDefaultClass \Stadly\PasswordPolice\CodeMap\LeetspeakMap
+ * @covers ::<private>
+ * @covers ::<protected>
+ * @covers ::__construct
+ */
+final class LeetspeakMapTest extends TestCase
+{
+    /**
+     * @covers ::getMap
+     */
+    public function testCanGetEncodeMap(): void
+    {
+        $codeMap = new LeetspeakMap(/*encode*/true);
+
+        $charTree = CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString('123'),
+            CharTree::fromString('áÁ'),
+            CharTree::fromString('æ'),
+            CharTree::fromString('Øô'),
+            CharTree::fromString('Ëñ'),
+        ]);
+        self::assertEquals([
+            'f' => ['f', 'ƒ'],
+            'B' => ['B', '8', 'ß'],
+            '1' => ['1'],
+            'á' => ['á'],
+            'æ' => ['æ'],
+            'Ø' => ['Ø'],
+            'Ë' => ['Ë'],
+        ], $codeMap->getMap($charTree), '', 0, 10, true);
+    }
+
+    /**
+     * @covers ::getMap
+     */
+    public function testCanGetDecodeMap(): void
+    {
+        $codeMap = new LeetspeakMap(/*encode*/false);
+
+        $charTree = CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString('123'),
+            CharTree::fromString('áÁ'),
+            CharTree::fromString('æ'),
+            CharTree::fromString('Øô'),
+            CharTree::fromString('Ëñ'),
+        ]);
+        self::assertEquals([
+            'f' => ['f'],
+            'B' => ['B'],
+            '1' => ['1', 'L', 'I'],
+            'á' => ['á'],
+            'æ' => ['æ'],
+            'Ø' => ['Ø'],
+            'Ë' => ['Ë'],
+        ], $codeMap->getMap($charTree), '', 0, 10, true);
+    }
+}
