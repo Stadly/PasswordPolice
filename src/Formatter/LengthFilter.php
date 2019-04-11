a3d2f3fa6162ed7740e91bd999cf7bc0a8237923
Stadly <magnar@myrtveit.com>
2019-04-11 17:15:07 +0200

    Length filter working on character trees

---
+++ src/Formatter/LengthFilter.php
@@ -0,0 +1,135 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\Formatter;
+
+use ArrayObject;
+use InvalidArgumentException;
+use SplObjectStorage;
+use Stadly\PasswordPolice\CharTree;
+use Stadly\PasswordPolice\Formatter;
+
+final class LengthFilter implements Formatter
+{
+    use Chaining;
+
+    /**
+     * @var int Minimum string length.
+     */
+    private $minLength;
+
+    /**
+     * @var int|null Maximum string length.
+     */
+    private $maxLength;
+
+    /**
+     * @var SplObjectStorage Memoization for already filtered character trees.
+     */
+    private $filterMemoization;
+
+    /**
+     * @param int $minLength Minimum string length.
+     * @param int|null $maxLength Maximum string length.
+     */
+    public function __construct(int $minLength = 0, ?int $maxLength = null)
+    {
+        if ($minLength < 0) {
+            throw new InvalidArgumentException('Min length must be non-negative.');
+        }
+        if ($maxLength !== null && $maxLength < $minLength) {
+            throw new InvalidArgumentException('Max length cannot be smaller than min length.');
+        }
+
+        $this->minLength = $minLength;
+        $this->maxLength = $maxLength;
+        $this->filterMemoization = new SplObjectStorage();
+    }
+
+    /**
+     * @param CharTree $charTree Character tree to format.
+     * @return CharTree Length filtered variant of the character tree.
+     */
+    protected function applyCurrent(CharTree $charTree): CharTree
+    {
+        return $this->applyInternal($charTree, $this->minLength, $this->maxLength);
+    }
+
+    /**
+     * @param CharTree $charTree Character tree to format.
+     * @param int $minLength Minimum string length.
+     * @param int|null $maxLength Maximum string length.
+     * @return CharTree Length filtered variant of the character tree.
+     */
+    private function applyInternal(CharTree $charTree, int $minLength, ?int $maxLength): CharTree
+    {
+        if (!isset($this->filterMemoization[$charTree])) {
+            $this->filterMemoization[$charTree] = new ArrayObject();
+        }
+
+        $memoization1 = $this->filterMemoization[$charTree];
+        if (!isset($memoization1[$minLength])) {
+            $memoization1[$minLength] = new ArrayObject();
+        }
+
+        $memoization2 = $memoization1[$minLength];
+        if (!isset($memoization2[$maxLength ?? ''])) {
+            $memoization2[$maxLength ?? ''] = $this->filter($charTree, $minLength, $maxLength);
+        }
+
+        return $memoization2[$maxLength ?? ''];
+    }
+
+    /**
+     * @param CharTree $charTree Character tree to format.
+     * @param int $minLength Minimum string length.
+     * @param int|null $maxLength Maximum string length.
+     * @return CharTree Length filtered variant of the character tree. Memoization is not used.
+     */
+    private function filter(CharTree $charTree, int $minLength, ?int $maxLength): CharTree
+    {
+        if ($minLength === 0 && $maxLength === null) {
+            return $charTree;
+        }
+
+        $root = $charTree->getRoot();
+
+        if ($root === null) {
+            return $charTree;
+        }
+
+        $branches = $charTree->getBranches();
+
+        if ($maxLength === 0) {
+            if ($root === '' && ($branches === [] || isset($branches[null]))) {
+                return CharTree::fromString('');
+            }
+            return CharTree::fromNothing();
+        }
+
+        $rootLength = mb_strlen($root);
+        $branchMinLength = $minLength <= $rootLength ? 0 : $minLength-$rootLength;
+        $branchMaxLength = $maxLength === null ? null : $maxLength-$rootLength;
+
+        $filteredBranches = [];
+        foreach ($branches as $branch) {
+            if ($branch->getRoot() === null) {
+                if ($minLength <= $rootLength) {
+                    $filteredBranches[] = $branch;
+                }
+            } elseif ($rootLength < $maxLength) {
+                $filteredBranch = $this->applyInternal($branch, $branchMinLength, $branchMaxLength);
+                if ($filteredBranch->getRoot() !== null) {
+                    $filteredBranches[] = $filteredBranch;
+                }
+            }
+        }
+
+        if ($filteredBranches !== [] || ($minLength <= $rootLength && $branches === [])) {
+            return CharTree::fromString($root, $filteredBranches);
+        }
+
+        return CharTree::fromNothing();
+    }
+}
---
+++ tests/Formatter/LengthFilterTest.php
@@ -0,0 +1,183 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stadly\PasswordPolice\Formatter;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use Stadly\PasswordPolice\CharTree;
+use Stadly\PasswordPolice\Formatter;
+
+/**
+ * @coversDefaultClass \Stadly\PasswordPolice\Formatter\LengthFilter
+ * @covers ::<private>
+ * @covers ::<protected>
+ * @covers ::__construct
+ */
+final class LengthFilterTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     * @doesNotPerformAssertions
+     */
+    public function testCanConstructFormatterWithMinLengthConstraintEqualToZero(): void
+    {
+        $formatter = new LengthFilter(0, null);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCannotConstructFormatterWithNegativeMinLengthConstraint(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $formatter = new LengthFilter(-10, null);
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testCannotConstructFormatterWithMaxLengthConstraintSmallerThanMinLengthConstraint(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $formatter = new LengthFilter(10, 5);
+    }
+
+    /**
+     * @covers ::__construct
+     * @doesNotPerformAssertions
+     */
+    public function testCanConstructFormatterWithMinLengthConstraintEqualToMaxLengthConstraint(): void
+    {
+        $formatter = new LengthFilter(5, 5);
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testTheSameIsReturnedWhenAnythingIsAllowed(): void
+    {
+        $formatter = new LengthFilter(0, null);
+
+        $charTree = CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('Ba'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString(''),
+            CharTree::fromString('1'),
+            CharTree::fromString('123'),
+        ]);
+        self::assertSame($charTree, $formatter->apply($charTree));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testTheSameIsReturnedWhenFilteringNull(): void
+    {
+        $formatter = new LengthFilter(0, 2);
+
+        $charTree = CharTree::fromNothing();
+        self::assertSame($charTree, $formatter->apply($charTree));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testEmptyStringIsReturnedWhenMaxLenghtIsNullAndCharTreeContainsEmptyString(): void
+    {
+        $formatter = new LengthFilter(0, 0);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString(''),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('Ba'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString(''),
+            CharTree::fromString('1'),
+            CharTree::fromString('123'),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testNothingIsReturnedWhenMaxLenghtIsNullAndCharTreeDoesNotContainEmptyString(): void
+    {
+        $formatter = new LengthFilter(0, 0);
+
+        self::assertEquals(CharTree::fromArray([
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('Ba'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString('1'),
+            CharTree::fromString('123'),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanFormatCharacterTree(): void
+    {
+        $formatter = new LengthFilter(1, 2);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString('Ba'),
+            CharTree::fromString('1'),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('Ba'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString(''),
+            CharTree::fromString('1'),
+            CharTree::fromString('123'),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanApplyFormatterChain(): void
+    {
+        $formatter = new LengthFilter(2, 3);
+
+        $next = $this->createMock(Formatter::class);
+        $next->method('apply')->willReturnCallback(
+            static function (CharTree $charTree): CharTree {
+                $charTrees = [];
+                foreach ($charTree as $string) {
+                    $charTrees[] = CharTree::fromString(strrev($string));
+                }
+                return CharTree::fromArray($charTrees);
+            }
+        );
+
+        $formatter->setNext($next);
+
+        self::assertEquals(CharTree::fromArray([
+            CharTree::fromString('oOf'),
+            CharTree::fromString('RaB'),
+            CharTree::fromString('zaB'),
+            CharTree::fromString('aB'),
+            CharTree::fromString('321'),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString('fOo'),
+            CharTree::fromString('Ba'),
+            CharTree::fromString('BaR'),
+            CharTree::fromString('Baz'),
+            CharTree::fromString(''),
+            CharTree::fromString('1'),
+            CharTree::fromString('123'),
+        ])));
+    }
+}
