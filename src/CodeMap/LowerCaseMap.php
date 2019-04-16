248a31f27ad293b276bd94caf22944c4dfdd7b5d
Stadly <magnar@myrtveit.com>
2019-04-16 09:14:56 +0200

    Use truncator in lower case map

--- src/CodeMap/LowerCaseMap.php
+++ src/CodeMap/LowerCaseMap.php
@@ -6,16 +6,29 @@ namespace Stadly\PasswordPolice\CodeMap;
 
 use Stadly\PasswordPolice\CharTree;
 use Stadly\PasswordPolice\CodeMap;
+use Stadly\PasswordPolice\Formatter\LengthFilter;
+use Stadly\PasswordPolice\Formatter\Truncator;
 
 final class LowerCaseMap implements CodeMap
 {
+    /**
+     * @var Truncator Formatter for extracting the first character.
+     */
+    private $charExtractor;
+
+    public function __construct()
+    {
+        $this->charExtractor = new Truncator(1);
+        $this->charExtractor->setNext(new LengthFilter(1));
+    }
+
     /**
      * {@inheritDoc}
      */
     public function getMap(CharTree $charTree): array
     {
         $codeMap = [];
-        foreach ($charTree->getTreeTrimmedToLength(1) as $char) {
+        foreach ($this->charExtractor->apply($charTree) as $char) {
             $codeMap[$char] = [mb_strtolower($char)];
         }
         return $codeMap;
