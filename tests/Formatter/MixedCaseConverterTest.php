f97800589b6eedc9aad52ad34d71297ed3e5da54
Stadly <magnar@myrtveit.com>
2019-04-16 09:44:38 +0200

    Test mixed case converter with empty and empty string character tree

--- tests/Formatter/MixedCaseConverterTest.php
+++ tests/Formatter/MixedCaseConverterTest.php
@@ -19,6 +19,32 @@ final class MixedCaseConverterTest extends TestCase
     /**
      * @covers ::apply
      */
+    public function testCanFormatEmptyCharacterTree(): void
+    {
+        $formatter = new MixedCaseConverter();
+
+        self::assertSame(CharTree::fromArray([
+        ]), $formatter->apply(CharTree::fromArray([
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
+    public function testCanFormatEmptyStringCharacterTree(): void
+    {
+        $formatter = new MixedCaseConverter();
+
+        self::assertSame(CharTree::fromArray([
+            CharTree::fromString(''),
+        ]), $formatter->apply(CharTree::fromArray([
+            CharTree::fromString(''),
+        ])));
+    }
+
+    /**
+     * @covers ::apply
+     */
     public function testCanFormatCharacterPath(): void
     {
         $formatter = new MixedCaseConverter();
