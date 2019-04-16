db352a82af2fb12334a795171174543124dd2637
Stadly <magnar@myrtveit.com>
2019-04-16 09:16:12 +0200

    Use truncator in mixed case map

--- src/CodeMap/MixedCaseMap.php
+++ src/CodeMap/MixedCaseMap.php
@@ -6,16 +6,29 @@ namespace Stadly\PasswordPolice\CodeMap;
 
 use Stadly\PasswordPolice\CharTree;
 use Stadly\PasswordPolice\CodeMap;
+use Stadly\PasswordPolice\Formatter\LengthFilter;
+use Stadly\PasswordPolice\Formatter\Truncator;
 
 final class MixedCaseMap implements CodeMap
 {
+    /**
+     * @var Truncator Formatter for extracting the first character.
+     */
+    private $charExtractor;
+
+    public function __construct()
+    {
+        $this->charExtractor = new Truncator(1);
+        $this->charExtractor->setNext(new LengthFilter(1));
+    }
+
     /**
      * {@inheritDoc}
      */
     public function getMap(CharTree $charTree): array
     {
         $codeMap = [];
-        foreach ($charTree->getTreeTrimmedToLength(1) as $char) {
+        foreach ($this->charExtractor->apply($charTree) as $char) {
             $lowerCase = mb_strtolower($char);
             $upperCase = mb_strtoupper($char);
 
